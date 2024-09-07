4f9ebb9776e8b723e0eff4d69d1416810348bbd8
Randy Polanco <70306858+randyp03@users.noreply.github.com>
2024-09-06 20:05:41 -0400

    Add net income visual

--- data_visuals.py
+++ data_visuals.py
@@ -2,35 +2,54 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-def draw_cat_dist(df):
+def draw_net_income(df):
+    net_income_df = df.copy()
+    net_income_df.loc[net_income_df['Category'] != 'Income', 'Amount'] *= -1
+    net_income_df = net_income_df.groupby(['Month'])[['Amount']].sum().sort_values('Amount', ascending=False)
+    net_income_df['Net_Status'] = ['Negative' if x < 0 else 'Positive' for x in net_income_df['Amount']]
+    palette = {'Positive': '#4bd02b', 'Negative': '#e33434'}
+
+    fig, ax = plt.subplots()
+    sns.barplot(net_income_df,
+                x='Month',
+                y='Amount',
+                hue = 'Net_Status',
+                palette=palette,
+                legend=False)
+    ax.set_title('Net Income')
+    ax.set_xlabel('Month')
+    ax.set_ylabel('Amount')
+    plt.tight_layout()
+
+    return plt.show()
+
+def draw_categorical_expenses(df):
     fig, ax = plt.subplots()
     sns.barplot(df.loc[df['Category'] != 'Income'].groupby('Category')[['Amount']].sum().sort_values('Amount', ascending=False),
                 x='Amount',
                 y='Category',
                 orient='h')
-    ax.set_title('Categorical Distribution')
+    ax.set_title('Categorical Expenses')
     ax.set_xlabel('Amount')
     ax.set_ylabel('Category')
     plt.tight_layout()
 
     return plt.show()
 
-
-def draw_monthly_plot(df):
-    fig, ax = plt.subplots(figsize=(13,8))
+def draw_monthly_expenditure(df):
+    fig, ax = plt.subplots(figsize=(10,6))
     sns.barplot(df.loc[df['Category'] != 'Income'].groupby(['Month','Sub-Category'])[['Amount']].sum().sort_values('Amount', ascending=False),
                 x='Amount',
                 y='Month',
                 hue="Sub-Category",
                 dodge=False,
                 orient='h')
-    ax.set_title('Categorical Distribution')
+    ax.set_title('Monthly Expenditure')
     ax.set_xlabel('Amount')
     ax.set_ylabel('Category')
     plt.tight_layout()
 
     return plt.show()
-
 
 def draw_cumsum_plot(df):
     df=df.loc[df['Category'] != 'Income']
@@ -57,10 +76,12 @@ def plot(PLOTS, df):
         if choice == 0:
             return
         elif choice == 1:
-            draw_cat_dist(df)
+            draw_net_income(df)
         elif choice == 2:
-            draw_monthly_plot(df)
+            draw_categorical_expenses(df)
         elif choice == 3:
+            draw_monthly_expenditure(df)
+        elif choice == 4:
             draw_cumsum_plot(df)
     except KeyError:
         print('\nInvalid option. Please enter an option from the available list.\n')
@@ -76,9 +97,10 @@ def main(csv_file):
 
     PLOTS = {
         0: 'Exit',
-        1: 'Categorical Expenses',
-        2: 'Monthy Expenditure',
-        3: 'Cumulative Sum of Dollars Spent',
+        1: 'Monthly Net Income',
+        2: 'Categorical Expenses',
+        3: 'Monthy Expenditure',
+        4: 'Cumulative Sum of Dollars Spent',
     }
 
     plot(PLOTS, visuals_df)
